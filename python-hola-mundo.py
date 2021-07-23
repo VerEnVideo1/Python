f09826c5bd277503b4e78c90644b7f2b15ee736a
VerEnVideo1 <49577481+VerEnVideo1@users.noreply.github.com>
2021-07-22 19:51:18 -0500

    Add files via upload

---
+++ python-hola-mundo.py
@@ -0,0 +1,40 @@
+
+print("Hola, ¿cuál es tu nombre?:")
+nombre = input()
+
+print("¡Hola! ", nombre )
+
+print("Este programa ejecuta operaciones aritméticas.")
+
+print("¿Qué número deseas utilizar:?")
+numero1 =  int(input())
+
+print("Ingresa el otro número:")
+numero2 =  int(input())
+
+print("La respuesta es:")
+print("El producto es:")
+print(numero1 * numero2)
+
+print("El Total es:")
+print(numero1 + numero2)
+
+print("La diferencia es:")
+print(numero1 - numero2)
+
+print("El cociente es:")
+print(numero1 / numero2)
+
+print("¡Adios! ", nombre, " regresa pronto." )
+
+#Función print
+#Función int
+#Función input
+
+#Operadores aritméticos
+#Para multiplicar: *
+#Para sumar: +
+#Para diferencia: -
+#Para división: /
+
+
